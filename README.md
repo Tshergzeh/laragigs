bc4ab3c09d8cd2d7a424a1e8cea33a03e3517809
Oluwasegun Ige <40037490+Tshergzeh@users.noreply.github.com>
2022-06-11 13:07:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# laragigs
+ A laravel project for creating and viewing job listings
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Listing;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory(10)->create();
+
+        Listing::factory(6)->create();
+
+        // Listing::create([
+        //     'title' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'email@email.com',
+        //     'website' => 'https://www.acme.com',
+        //     'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum minus neque sint, culpa voluptate ipsa sapiente earum tempora consequuntur odio consectetur quo quisquam. Totam atque mollitia quo natus dicta enim.'
+        // ]);
+
+        // Listing::create([
+        //     'title' => 'Full-Stack Engineer',
+        //     'tags' => 'laravel, backend, api',
+        //     'company' => 'Aspire Technologies',
+        //     'location' => 'Ilorin, Nigeria',
+        //     'email' => 'info@aspiretech.com',
+        //     'website' => 'https://aspiretech.com',
+        //     'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum minus neque sint, culpa voluptate ipsa sapiente earum tempora consequuntur odio consectetur quo quisquam. Totam atque mollitia quo natus dicta enim.'
+        // ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+    }
+}
